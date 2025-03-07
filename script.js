e254c0f17d8600528bc12194f1f30cc90fb28d16
Maxibiznes <129507354+Maxibiznes@users.noreply.github.com>
2025-03-07 17:07:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function() {  
+﻿window.onload = function() {  
   let dateInput = document.getElementById("date");  
   let today = new Date();  
   let dd = String(today.getDate()).padStart(2, '0');  
@@ -20,7 +20,7 @@ window.onload = function() {
       .then(data => {
         // Фільтрація записів за вибраною датою
         const bookedTimes = data.filter(row => row[0] === dateInput).map(row => row[1]);
-        const times = ['10:00', '12:00', '14:00', '16:00', '18:00'];
+        const times = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30', '13:00', '13:30', '14:00', '14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30','20:00'];
         times.forEach(time => {
           const option = document.createElement('option');
           option.value = time;
